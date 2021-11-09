7133a456b3174612ee4b9eb5276cd94934d45be9
DyatlovIvan <diatlovivan92@mail.ru>
2021-11-09 17:30:38 +0400

    create structure portfolio

---
+++ src/nav/Nav.js
@@ -0,0 +1,14 @@
+import style from './Nav.module.css'
+import React from "react";
+
+export const Nav = () =>{
+    return(
+        <div className={style.nav}>
+            <a href="">Главная</a>
+            <a href="">Скилы</a>
+            <a href="">Проекты</a>
+            <a href="">Контакты</a>
+        </div>
+
+    )
+}
