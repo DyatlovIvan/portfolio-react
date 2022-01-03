de8fc4fb4a5136e5503dc20d1b44d65ffe3d6408
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-03 22:19:01 +0400

    header scss

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,14 @@
-import style from './Nav.module.css'
+import style from './Nav.module.scss'
 import React from "react";
 
 export const Nav = () =>{
     return(
         <div className={style.nav}>
-            <a href="">Главная</a>
-            <a href="">Скилы</a>
-            <a href="">Проекты</a>
-            <a href="">Контакты</a>
+
+            <a href="" className={style.navText}>Main</a>
+            <a href="" className={style.navText}>Skills</a>
+            <a href="" className={style.navText}>Projects</a>
+            <a href="" className={style.navText}>Contacts</a>
         </div>
 
     )
