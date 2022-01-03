ef051da94c59bc31251dd1792d6a97d7d341f153
DyatlovIvan <diatlovivan92@mail.ru>
2022-01-03 21:59:09 +0400

    main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,21 @@
 import React from "react";
 import style from './Main.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
+import avatarImage from '../assets/image/photo.jpg'
 
 export const Main = () => {
+    const avatar = {
+        backgroundImage:`url(${avatarImage})`
+    }
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={style.text}>
-                    <span>HELLO! I AM</span>
-                    <h1>DYATLOV IVAN</h1>
-                    <p>Frontend Developer.</p>
+                    <span className={`${style.discription} ${style.primaryText}`}>HELLO! I AM</span>
+                    <h1 className={style.textName}>DYATLOV IVAN</h1>
+                    <p className={style.discription}>Frontend Developer.</p>
                 </div>
-                <div className={style.photo}>
+                <div className={style.photo} style={avatar}>
 
                 </div>
             </div>
